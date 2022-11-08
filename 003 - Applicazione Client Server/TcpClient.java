7f4b4cdac854dcb0b3a474f17ede9a348c4a5db8
QuagliataAlberto <113895221+QuagliataAlberto@users.noreply.github.com>
2022-11-08 11:22:26 +0100

    Update and rename Tcpclient.java to TcpClient.java

--- 003 - Applicazione Client Server/TcpClient.java
+++ 003 - Applicazione Client Server/TcpClient.java
@@ -1,8 +1,3 @@
-/**
- * from network/..
- * javac network/TcpClient.java
- * java network.TcpClient 
- */
 package network;
 
 import java.io.DataInputStream;
@@ -14,7 +9,7 @@ import java.net.Socket;
 
 public class TcpClient {
 	public static void main(String[] args) throws Exception {
-		
+
 		String severAddress="127.0.0.1";  // localhost
 		int severPort=8765;
 		String clientMsg = "";
@@ -23,16 +18,16 @@ public class TcpClient {
 		try {
 			// Create connection to server socket
 			System.out.print("Client: Connessione al server=" + severAddress + ":" + severPort + " ... ");
-			Socket socket = new Socket(severAddress, severPort); 
+			Socket socket = new Socket(severAddress, severPort);
 			System.out.println("Connected");
-
+		
 			// Create input and output streams to read/write data
-			// Input stream tramite la classe java Scanner per i dati inseriti dall'utente 
+			// Input stream tramite la classe java Scanner per i dati inseriti dall'utente
 			BufferedReader inUserStream = new BufferedReader(new InputStreamReader(System.in));
 			//Scanner scanner = new Scanner(System.in);
-			// Input stream per i dati provenienti dal socket 
+			// Input stream per i dati provenienti dal socket
 			DataInputStream inSocketStream = new DataInputStream(socket.getInputStream());
-			// Output stream 
+			// Output stream
 			DataOutputStream outSocketStream = new DataOutputStream(socket.getOutputStream());
 			
 			while (!clientMsg.equals("quit")) {
@@ -40,25 +35,28 @@ public class TcpClient {
 				System.out.print("Client: inserisci il messaggio da inviare> ");
 				//clientMsg = scanner.nextLine();
 				clientMsg = inUserStream.readLine();
-
+				
 				// Send the entered text to server
 				System.out.println("Client: invio il messaggio: " + clientMsg);
 				outSocketStream.writeUTF(clientMsg);
 				outSocketStream.flush();
-
+				
 				// Read data from socket input stream
 				serverMsg = inSocketStream.readUTF();
 				System.out.println("Client: ricevuto il messaggio: " + serverMsg);
+				
 			}
-
-			// Close resources
-			outSocketStream.close();
-			inSocketStream.close();
-			inUserStream.close();
-			socket.close();
-			//scanner.close();			
-		} catch (Exception e) {
-			System.out.println(e);
+				
+				// Close resources
+				outSocketStream.close();
+				inSocketStream.close();
+				inUserStream.close();
+				socket.close();
+				//scanner.close();
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+		
+			}
 		}
-	}
-}
+				
